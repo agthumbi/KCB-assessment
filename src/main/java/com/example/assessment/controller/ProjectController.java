d2dd11a73e66f8d0a968fe7cc1b5f37db0d3ee61
KENCONT2323 <KENCONT2323@kcbgroup.com>
2024-12-03 13:01:31 +0300

    Pacthed Validation

--- src/main/java/com/example/assessment/controller/ProjectController.java
+++ src/main/java/com/example/assessment/controller/ProjectController.java
@@ -4,11 +4,20 @@ package com.example.assessment.controller;
 import com.example.assessment.models.Project;
 import com.example.assessment.models.Task;
 import com.example.assessment.services.ProjectService;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/projects")
@@ -47,6 +56,35 @@ public class ProjectController {
         return projectService.getRetrieveAllTasks(projectId);
 
     }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
 
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Map<String, String> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        if (!ex.getConstraintViolations().isEmpty()) {
+            for (ConstraintViolation constraintViolation : ex.getConstraintViolations()) {
+                String fieldName = null;
+                for (Path.Node node : constraintViolation.getPropertyPath()) {
+                    fieldName = node.getName();
+                }
+                errors.put(fieldName, constraintViolation.getMessage());
+            }
+        }
+
+        return errors;
+    }
 
 }
--- src/main/java/com/example/assessment/controller/TasksController.java
+++ src/main/java/com/example/assessment/controller/TasksController.java
@@ -2,9 +2,18 @@ package com.example.assessment.controller;
 
 import com.example.assessment.models.Task;
 import com.example.assessment.services.TasksService;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -20,5 +29,35 @@ public class TasksController {
     public ResponseEntity<?> getDeleteTask(@PathVariable("taskid") Long taskid){
         return tasksService.getDeleteTask(taskid);
 
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Map<String, String> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        if (!ex.getConstraintViolations().isEmpty()) {
+            for (ConstraintViolation constraintViolation : ex.getConstraintViolations()) {
+                String fieldName = null;
+                for (Path.Node node : constraintViolation.getPropertyPath()) {
+                    fieldName = node.getName();
+                }
+                errors.put(fieldName, constraintViolation.getMessage());
+            }
+        }
+
+        return errors;
     }
 }
